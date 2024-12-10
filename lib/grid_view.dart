0d11874e9d8d831688af0a663a9faeadaf2ec8db
Farid322 <faridmahmoud329@gmail.com>
2024-12-10 18:33:26 +0200

    update

--- lib/grid_view.dart
+++ lib/grid_view.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:task_nti/core/app_colors.dart';
 import 'package:task_nti/models/letter_model.dart';
 import 'package:task_nti/widgets/container.dart';
+
 class Screen extends StatelessWidget {
-   Screen({super.key});
+  Screen({super.key});
 
 //  final List<String> Letters = [
-//   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
-//   'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+//   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+//   'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
 //   'u', 'v', 'w', 'x', 'y', 'z','0','1','2','3','4','5'];
 //  final List<Color> colors = [
 //   AppColors.n1,
@@ -43,26 +44,55 @@ class Screen extends StatelessWidget {
 //   AppColors.n31,
 //   AppColors.n32,
 // ];
- 
- final List <LetterModel> lettermodel = [
-  LetterModel(color:  AppColors.n1,  title: 'a'),
-  
- ];
- 
+
+  final List<LetterModel> lettermodel = [
+    LetterModel(color: AppColors.n1, title: 'a'),
+    LetterModel(color: AppColors.n2, title: 'b'),
+    LetterModel(color: AppColors.n3, title: 'c'),
+    LetterModel(color: AppColors.n4, title: 'd'),
+    LetterModel(color: AppColors.n5, title: 'e'),
+    LetterModel(color: AppColors.n6, title: 'f'),
+    LetterModel(color: AppColors.n7, title: 'g'),
+    LetterModel(color: AppColors.n8, title: 'h'),
+    LetterModel(color: AppColors.n9, title: 'i'),
+    LetterModel(color: AppColors.n10, title: 'j'),
+    LetterModel(color: AppColors.n11, title: 'k'),
+    LetterModel(color: AppColors.n12, title: 'l'),
+    LetterModel(color: AppColors.n13, title: 'm'),
+    LetterModel(color: AppColors.n14, title: 'n'),
+    LetterModel(color: AppColors.n15, title: 'o'),
+    LetterModel(color: AppColors.n16, title: 'p'),
+    LetterModel(color: AppColors.n17, title: 'q'),
+    LetterModel(color: AppColors.n18, title: 'r'),
+    LetterModel(color: AppColors.n19, title: 's'),
+    LetterModel(color: AppColors.n20, title: 't'),
+    LetterModel(color: AppColors.n21, title: 'u'),
+    LetterModel(color: AppColors.n22, title: 'v'),
+    LetterModel(color: AppColors.n23, title: 'w'),
+    LetterModel(color: AppColors.n24, title: 'x'),
+    LetterModel(color: AppColors.n25, title: 'y'),
+    LetterModel(color: AppColors.n26, title: 'z'),
+    LetterModel(color: AppColors.n27, title: ''),
+    LetterModel(color: AppColors.n28, title: ''),
+    LetterModel(color: AppColors.n29, title: '0'),
+    LetterModel(color: AppColors.n30, title: '1'),
+    LetterModel(color: AppColors.n31, title: '2'),
+    LetterModel(color: AppColors.n32, title: '3'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold (
-       body: GridView.builder(
-        gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 4 ,
-        crossAxisSpacing: 3,
-        mainAxisSpacing: 5) ,
-        itemCount:lettermodel.length ,
+    return Scaffold(
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4, crossAxisSpacing: 3, mainAxisSpacing: 5),
+        itemCount: lettermodel.length,
         itemBuilder: (context, index) {
-         return GridViewItem(color: lettermodel[index].color, letter:lettermodel[index].title ); 
+          return GridViewItem(
+              color: lettermodel[index].color,
+              letter: lettermodel[index].title);
         },
-       ),
-    
+      ),
     );
   }
 }
